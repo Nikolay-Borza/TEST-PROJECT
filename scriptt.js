07ca935a619cad0e7310aaf4485368d3ebd43ad0
Nikolay <example@gmail.com>
2021-01-12 15:45:25 +0500

    8 урок Условия

---
+++ scriptt.js
@@ -0,0 +1,41 @@
+// if (2*4 == 8) {
+//   console.log("Верно!")
+// }  else {
+//   console.log("Неверно!")
+// }
+
+// УСЛОВИЯ!
+
+let num = 50;
+
+if (num < 49) {
+    console.log ("Неверно")
+}  else if (num > 100) {
+    console.log ("Много")
+}  else {
+    console.log("Верно!")
+};
+
+(num == 50) ? console.log("Верно!") : console.log ("Неверно");
+   
+
+switch (num) {
+    case num < 49:
+        console.log ("Неверно");
+        break;
+
+    case num > 100:
+        console.log ("Много");
+        break;  
+
+    case num > 80:
+        console.log ("Всё еще Много");
+        break;    
+
+    case 50:
+        console.log("Верно!") 
+        break;  
+    default:
+        console.log("Что то пошло не так!"); 
+        break;        
+}  
